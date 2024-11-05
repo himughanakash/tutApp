8fbcef23253daef7734b6be5b8a9d94482b2bdb9
himughanakash <himu0501@gmail.com>
2024-11-05 10:23:15 +0530

    attribute-directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AddEmployeeComponent } from './components/add-employee/add-employee.com
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { StructuralDirectiveComponent } from './components/Directives/structural-directive/structural-directive.component';
+import { AttributeDirectiveComponent } from './components/Directives/attribute-directive/attribute-directive.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DataBindingComponent, StructuralDirectiveComponent],
+  imports: [RouterOutlet, DataBindingComponent, StructuralDirectiveComponent, AttributeDirectiveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/Directives/attribute-directive/attribute-directive.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-attribute-directive',
+  imports:[CommonModule],
+  templateUrl: './attribute-directive.component.html',
+  styleUrls: ['./attribute-directive.component.css']
+})
+export class AttributeDirectiveComponent implements OnInit {
+  div1bgColor: string = "bg-primary";
+  isDiv2Active: boolean = false;
+
+  addRedColor(){
+    this.div1bgColor = "bg-danger"
+  }
+  addBlueColor(){
+    this.div1bgColor = "bg-primary"
+  }
+
+  changeValue(){
+    if(this.isDiv2Active == true){
+      this.isDiv2Active = false;
+    }else{
+      this.isDiv2Active = true;
+    }
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
